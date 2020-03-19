7fcafad5dbb54b84681c2078ba9625cccefc74e8
caolingyi <1318840626@qq.com>
2020-03-19 11:47:27 +0800

    fix

---
+++ src/OKEX.Auto.TradeAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OKEX.Auto.TradeAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
